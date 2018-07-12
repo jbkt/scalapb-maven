345f49f390bae6e7feb4c14bf5d59ac0d6f5e290
norbjd <norbjd@users.noreply.github.com>
2018-07-12 15:17:41 +0200

    Add parameters to the plugin (input/output, Java compatibility, ...)
    - Update scalapb version to the latest release (0.7.4)
    - Add configuration parameters for I/O : input directory, include
    directories, output directory
    - List recursively all proto files in input directory
    - Add flat_package flag
    - Add configuration parameters for Java compatibility (disabled by
    default)
    - Add documentation / update README.md

--- README.md
+++ README.md
@@ -3,11 +3,11 @@
 Add the following snippet to your pom.
 
 ```xml
-<plugin>
+<plugins>
   <plugin>
     <groupId>net.catte</groupId>
     <artifactId>scalapb-maven-plugin</artifactId>
-    <version>${project.version}</version>
+    <version>${scalapb-maven-plugin.version}</version>
     <executions>
       <execution>
         <goals>
@@ -17,5 +17,55 @@ Add the following snippet to your pom.
       </execution>
     </executions>
   </plugin>
-</plugin>
+</plugins>
+```
+
+### Configuration
+
+| Maven property       | Description                                | Default                                                 |
+| :------------------- | :----------------------------------------- | :------------------------------------------------------ |
+| `skip`               | `true` to skip protobuf compilation        | `false`                                                 |
+| `protocVersion`      | Protoc binary version                      | `v300`                                                  |
+| `inputDirectory`     | Input directory containing `*.proto` files | `${project.basedir}/src/main/protobuf`                  |
+| `includeDirectories` | Additional include directories (array)     | `[]`                                                    |
+| `outputDirectory`    | Output directory for Scala files           | `${project.build.directory}/generated-sources/protobuf` |
+| `flatPackage`        | `true` to flatten packages                 | `false`                                                 |
+
+For Java compatibility configuration, see the next section.
+
+### Java compatibility
+
+#### Configuration
+
+| Maven property        | Description                                        | Default                                                 |
+| :-------------------- | :------------------------------------------------- | :------------------------------------------------------ |
+| `javaOutput`          | `true` to also generate Java classes               | `false`                                                 |
+| `javaConversions`     | `true` to enable Java conversions in Scala classes | `false`                                                 |
+| `javaOutputDirectory` | Output directory for Java files                    | `${project.build.directory}/generated-sources/protobuf` |
+
+#### Usage
+
+To generate Java classes along with the Scala ones, use the following
+configuration.
+
+```xml
+<plugins>
+  <plugin>
+    <groupId>net.catte</groupId>
+    <artifactId>scalapb-maven-plugin</artifactId>
+    <version>${scalapb-maven-plugin.version}</version>
+    <configuration>
+      <javaOutput>true</javaOutput>
+      <javaConversions>true</javaConversions>
+    </configuration>
+    <executions>
+      <execution>
+        <goals>
+          <goal>compile</goal>
+        </goals>
+        <phase>generate-sources</phase>
+      </execution>
+    </executions>
+  </plugin>
+</plugins>
 ```
--- scalapb-maven-plugin/src/main/java/net/catte/scalapb/maven/plugin/CompileMojo.java
+++ scalapb-maven-plugin/src/main/java/net/catte/scalapb/maven/plugin/CompileMojo.java
@@ -1,14 +1,18 @@
 package net.catte.scalapb.maven.plugin;
 
+import org.apache.commons.lang3.ArrayUtils;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.project.MavenProject;
 
+import java.io.File;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Arrays;
 
 @Mojo(name = "compile")
 public class CompileMojo extends AbstractMojo {
@@ -19,24 +23,125 @@ public class CompileMojo extends AbstractMojo {
     @Parameter(defaultValue = "false")
     private boolean skip;
 
+    /**
+     * Protoc version.
+     * Defaults to v3.0.0
+     *
+     * @parameter property="protocVersion"
+     */
+    @Parameter(defaultValue = "v300")
+    private String protocVersion;
+
+    /**
+     * Input directory containing *.proto files.
+     * Defaults to <code>${project.basedir}/src/main/protobuf</code>.
+     *
+     * @parameter property="inputDirectory"
+     */
+    @Parameter(defaultValue = "${project.basedir}/src/main/protobuf")
+    private File inputDirectory;
+
+    /**
+     * Additional include directories.
+     *
+     * @parameter property="includeDirectories"
+     */
+    @Parameter
+    private File[] includeDirectories;
+
+    /**
+     * Output directory for Scala generated classes.
+     * Defaults to <code>${project.build.directory}/generated-sources/protobuf</code>.
+     *
+     * @parameter property="outputDirectory"
+     */
+    @Parameter(defaultValue = "${project.build.directory}/generated-sources/protobuf")
+    private File outputDirectory;
+
+    /**
+     * Set to true if output packages need to be flatten.
+     * Defaults to <code>false</code>.
+     *
+     * @parameter property="flatPackage"
+     */
+    @Parameter(defaultValue = "false")
+    private boolean flatPackage;
+
+    /**
+     * Set to true if Java classes also need to be generated.
+     * Defaults to <code>false</code>.
+     *
+     * @parameter property="javaOutput"
+     */
+    @Parameter(defaultValue = "false")
+    private boolean javaOutput;
+
+    /**
+     * Set to true if Java conversions are needed.
+     * Defaults to <code>false</code>.
+     *
+     * @parameter property="javaConversions"
+     */
+    @Parameter(defaultValue = "false")
+    private boolean javaConversions;
+
+    /**
+     * Output directory for Java generated classes.
+     * Defaults to <code>${project.build.directory}/generated-sources/protobuf</code>.
+     *
+     * @parameter property="javaOutputDirectory"
+     */
+    @Parameter(defaultValue = "${project.build.directory}/generated-sources/protobuf")
+    private File javaOutputDirectory;
+
     public void execute() throws MojoExecutionException {
         if (skip) {
             getLog().info("Skip flag set, skipping protobuf compilation.");
             return;
         }
 
-        String baseDir = project.getBasedir().getPath();
-        String buildDir = project.getBuild().getOutputDirectory();
-
-        Path protoPath= Paths.get(baseDir, "src", "main", "protobuf");
+        Path protoPath = Paths.get(inputDirectory.toURI());
         getLog().info("Reading proto files in '" + protoPath +"'.");
-        Path scalaOut = Paths.get(buildDir, "generated-sources","protobuf");
-        getLog().info("Writing Scala files in '" + scalaOut +"'.");
-        project.addCompileSourceRoot(scalaOut.toString());
+
+        Path[] includeDirectoriesPaths = ArrayUtils.toArray();
+        if(includeDirectories != null) {
+          includeDirectoriesPaths = Arrays.stream(includeDirectories).map(f -> {
+            getLog().info("Including proto files from '" + protoPath +"'.");
+            return Paths.get(f.toURI());
+          }).toArray(Path[]::new);
+        }
+
+        Path scalaOutPath = Paths.get(outputDirectory.toURI());
+        getLog().info("Writing Scala files in '" + scalaOutPath +"'.");
+        project.addCompileSourceRoot(scalaOutPath.toString());
+
+        Path javaOutPath = null;
+        if(javaOutput) {
+          javaOutPath = Paths.get(javaOutputDirectory.toURI());
+          getLog().info("Writing Java files in '" + javaOutPath +"'.");
+          project.addCompileSourceRoot(javaOutPath.toString());
+        }
 
         try {
-            Files.createDirectories(scalaOut);
-            ProtoCompiler.compile(protoPath, scalaOut);
+          Files.createDirectories(scalaOutPath);
+          if(javaOutput && javaOutPath != null) {
+            Files.createDirectories(javaOutPath);
+          }
+        } catch (IOException ioe) {
+          throw new MojoExecutionException("Error creating output directories", ioe);
+        }
+
+        try {
+            ProtoCompiler.compile(
+              protocVersion,
+              protoPath,
+              includeDirectoriesPaths,
+              scalaOutPath,
+              flatPackage,
+              javaConversions,
+              javaOutput,
+              javaOutPath
+            );
         } catch (Exception e) {
             throw new MojoExecutionException("Error compiling protobuf files", e);
         }
